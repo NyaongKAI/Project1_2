e42323612e3842b7e4e789d27d0f67b9e40b2e97
NyaongKAI <wx1997@naver.com>
2021-01-03 12:17:23 +0900

    두 번째 깃허브 등록 실험 프로젝트 2

---
+++ Project1_2/githubEx2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int dan = 0, num = 1;
+	printf("¸î ´Ü? ");
+	scanf("%d", &dan);
+
+	while (num < 10)
+	{
+		printf("%d x %d = %d \n", dan, num, dan * num);
+		num++;
+	}
+	return 0;
+}
